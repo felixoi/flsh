1543971126f50bb0ef69551536aeec910447d16b
Felix Kleinekathoefer <felix@felixoi.com>
2022-01-15 19:35:04 +0100

    Test API Requests

--- site/pages/admin/index.vue
+++ site/pages/admin/index.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
     Admin Area
+    {{ test }}
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: null,
+    }
+  },
+  mounted() {
+    this.fetchTest()
+  },
+  methods: {
+    async fetchTest() {
+      this.test = await (await fetch('https://link-dev.felixoi.com/api/urls', {
+        method: 'POST'
+      })).json()
+    }
+  }
+}
+</script>
--- site/pages/index.vue
+++ site/pages/index.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
     User Area
+    {{ test }}
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: null
+    }
+  },
+  mounted() {
+    this.fetchTest()
+  },
+  methods: {
+    async fetchTest() {
+      this.test = await (await fetch('https://link-dev.felixoi.com/api/urls/123')).json()
+      console.log(this.test)
+    }
+  }
+}
+</script>
